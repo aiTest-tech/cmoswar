170143daa16e2f89977247710c548debca25dac1
hplaptop <hplaptop@gmail.com>
2024-11-19 16:42:57 +0530

    testing

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -10,6 +10,7 @@ const Navbar = () => {
                         <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                             {/* Mobile menu button*/}
                             <button
+                                onClick={() => setishamburgarOpen(!ishamburgarOpen)}
                                 type="button"
                                 className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
                                 aria-controls="mobile-menu"
