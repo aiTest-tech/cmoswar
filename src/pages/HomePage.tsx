62174669e2aee7f7170680d56e005f8fcf7c6ef4
cmo <cmoaiteam@gmail.com>
2024-11-20 07:28:50 +0530

    layout created

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,23 @@
-import Navbar from "../component/Navbar"
+import Navbar from "../component/Navbar";
+import { AiFillAudio } from "react-icons/ai";
 const HomePage = () => {
-    return (
-        <>
-            <Navbar />
-        </>
-    )
-}
+  return (
+    <>
+      <Navbar />
+      <div className="relative top-16">
+        <div className="flex justify-between max-w-[99%] mx-auto gap-2 h-[90vh]  rounded-[3px]">
+          <div className="w-[20%] bg-[#121212] rounded-[10px] max-w-sm:w-[30%]">
+            <section className="max-w-[95%] bg-[#1f1f1f] h-[30%] mx-auto mt-14 rounded-[14px] flex justify-center items-center">
+                <div>
+                    <AiFillAudio className="w-[40px] h-[40px] rounded-full bg-white" />
+                </div>
+            </section>
+          </div>
+          <div className="w-[80%] bg-[#121212] rounded-[10px] max-w-sm:w-[70%]"></div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default HomePage
+export default HomePage;
